3649b0fddb4e9e25a99ba40379cb84c670df303f
ikota3 <51883336+ikota3@users.noreply.github.com>
2020-04-12 00:57:03 +0900

    Add binary2decimal.py

---
+++ python-algorithms/binary2decimal.py
@@ -0,0 +1,17 @@
+﻿
+def binary_to_decimal(binary_str):
+    result = 0
+    for i in range(len(binary_str)):
+        result += int(binary_str[i]) * (2 ** (len(binary_str) - i - 1))
+
+    return result
+
+
+if __name__ == '__main__':
+    input_str = input('Enter a binary number: ')
+    try:
+        binary_str = str(int(input_str))
+        result = binary_to_decimal(binary_str)
+        print(result)
+    except ValueError:
+        print('Please enter a number.')
