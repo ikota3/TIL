ca333f553ee853aa4b550c40eec2c6906f167fa7
ikota3 <51883336+ikota3@users.noreply.github.com>
2020-04-12 00:32:55 +0900

    Add decimal2binary.py

---
+++ python-algorithms/decimal2binary.py
@@ -0,0 +1,21 @@
+﻿
+def decimal_to_binary(decimal_number):
+    result = ''
+    if decimal_number == 0:
+        result = 0
+        return result
+
+    while decimal_number > 0:
+        result = str(decimal_number % 2) + result
+        decimal_number //= 2
+
+    return result
+
+
+if __name__ == '__main__':
+    input_str = input('Enter decimal number: ')
+    try:
+        result = decimal_to_binary(int(input_str))
+        print(result)
+    except ValueError:
+        print('Please enter a number.')
