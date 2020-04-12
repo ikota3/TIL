36833db0d5f025395fb48f07af79cb3883dcc608
ikota3 <51883336+ikota3@users.noreply.github.com>
2020-04-12 14:44:05 +0900

    Add is_prime.py

---
+++ python-algorithms/2-5/is_prime.py
@@ -0,0 +1,17 @@
+﻿import math
+
+
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if num % i == 0:
+            return False
+
+    return True
+
+
+if __name__ == '__main__':
+    for i in range(200):
+        if is_prime(i):
+            print(i, end=' ')
