81d6baa44da5c7ba882bf4e59b86ed4316f6b650
ikota3 <51883336+ikota3@users.noreply.github.com>
2020-04-12 15:26:33 +0900

    Add eratosthenes.py

---
+++ python-algorithms/2-5/eratosthenes.py
@@ -0,0 +1,26 @@
+﻿import math
+
+
+def get_prime(num):
+    if num <= 1:
+        return []
+    elif num == 2:
+        return [2]
+
+    prime = [2]
+    limit = int(math.sqrt(num))
+
+    # 奇数のリストを作成
+    data = [i + 1 for i in range(2, num, 2)]
+    # 検索上限 > 奇数リストにある最小の数
+    while limit > data[0]:
+        # 素数リストに追加
+        prime.append(data[0])
+        # 奇数リストを再作成
+        # 既存の奇数リストにある数の中で、それぞれを最小の数で割り切れない数を入れる
+        data = [j for j in data if j % data[0] != 0]
+
+    return prime + data
+
+
+print(get_prime(3))
