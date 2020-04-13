5c34e90a646cecd8420394c34ea97d757d04c33b
ikota3 <51883336+ikota3@users.noreply.github.com>
2020-04-14 00:20:48 +0900

    Add linear-search1

---
+++ python-algorithms/4-1/linear-search1.py
@@ -0,0 +1,10 @@
+﻿DATA = [50, 30, 90, 10, 20, 70, 60, 40, 80]
+found = False
+for i in range(len(DATA)):
+    if DATA[i] == 40:
+        print(i)
+        found = True
+        break
+
+if not found:
+    print('Not Found')
