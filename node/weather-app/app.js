d803e67b0e0f5e3df7c1b25c62ae8bb37d37e3de
ikota3 <51883336+ikota3@users.noreply.github.com>
2020-05-26 23:50:23 +0900

    Update weather-app

--- node/weather-app/app.js
+++ node/weather-app/app.js
@@ -1,28 +1,49 @@
+const yargs = require("yargs");
 const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
 
-const weatherKey = "";
-const weatherQuery = "37.8267,-122.4233";
-const weatherUrl = `http://api.weatherstack.com/current?access_key=${weatherKey}&query=${weatherQuery}&units=m`;
+/**
+ * Fetch weather data from address.
+ *
+ * @param {string} address
+ */
+function fetchWeatherData(address) {
+  geocode(address, (error, { latitude, longitude, location } = {}) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log(latitude, longitude, location);
 
-geocode.geocode("Fukuoka", (error, data) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-  console.log(data);
+    forecast(
+      latitude,
+      longitude,
+      (error, { weather_description, temperature, feelslike }) => {
+        if (error) {
+          return console.log(error);
+        }
+        console.log(
+          `${weather_description}. It is currently ${temperature} degrees out. It feels like ${feelslike} degrees out.`
+        );
+      }
+    );
+  });
+}
+
+// weather command
+// usage: node app.js weather --address="Tokyo"
+yargs.command({
+  command: "weather",
+  describe: "Show weather data from address.",
+  builder: {
+    address: {
+      describe: "address.",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler({ address }) {
+    fetchWeatherData(address);
+  },
 });
 
-// request({ url: weather_url, json: true }, (error, response, body) => {
-//   if (error) {
-//     console.log("Unable to connect weather stack.");
-//     return;
-//   }
-//   if (body.error) {
-//     console.log("Unable to find location.");
-//     return;
-//   }
-//   const currentData = body.current;
-//   console.log(
-//     `${currentData.weather_descriptions[0]}. It is currently ${currentData.temperature} degrees out. It feels like ${currentData.feelslike} degrees out.`
-//   );
-// });
+yargs.parse();
--- node/weather-app/utils/geocode.js
+++ node/weather-app/utils/geocode.js
@@ -1,12 +1,19 @@
 const request = require("postman-request");
 
+/**
+ * Get latitude and longitude from giving address.
+ * Returns latitude, longitude, location.
+ *
+ * @param {string} address
+ * @param {Function} callback
+ */
 const geocode = (address, callback) => {
   const accessKey = "";
   const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
     address
   )}.json?access_token=${accessKey}&limit=1`;
 
-  request({ url: url, json: true }, (error, response, body) => {
+  request({ url, json: true }, (error, { body }) => {
     if (error) {
       callback("Unable to connect to mapbox.", undefined);
       return;
@@ -24,6 +31,4 @@ const geocode = (address, callback) => {
   });
 };
 
-module.exports = { geocode };
-
-console.log(module.exports);
+module.exports = geocode;
