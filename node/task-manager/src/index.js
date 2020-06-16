288e6a074251bb1e92bcd83cfa52f82d1696a9d6
ikota3 <51883336+ikota3@users.noreply.github.com>
2020-06-16 23:25:20 +0900

    Add delete method for users and tasks

--- node/task-manager/src/index.js
+++ node/task-manager/src/index.js
@@ -65,6 +65,19 @@ app.patch("/users/:id", async (req, res) => {
   }
 });
 
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).send();
+    }
+
+    res.status(200).send(user);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
 app.post("/tasks", async (req, res) => {
   if (!req.body) {
     return res.status(400).send();
@@ -123,6 +136,18 @@ app.patch("/tasks/:id", async (req, res) => {
     res.status(200).send(task);
   } catch (e) {
     res.status(400).send(e);
+  }
+});
+
+app.delete("/tasks/:id", async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.status(200).send(task);
+  } catch (e) {
+    res.status(500).send(e);
   }
 });
 
