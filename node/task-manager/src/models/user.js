c20f5e4eea9d232cbc5df4559316a3372c89a55d
ikota3 <51883336+ikota3@users.noreply.github.com>
2020-06-21 22:03:26 +0900

    Override user toJSON method

--- node/task-manager/src/models/user.js
+++ node/task-manager/src/models/user.js
@@ -61,6 +61,15 @@ userSchema.methods.generateAuthToken = async function () {
   return token;
 };
 
+userSchema.methods.toJSON = function () {
+  const userObject = this.toObject();
+
+  delete userObject.password;
+  delete userObject.tokens;
+
+  return userObject;
+};
+
 userSchema.statics.findByCredentials = async (email, password) => {
   const user = await User.findOne({ email });
   if (!user) {
