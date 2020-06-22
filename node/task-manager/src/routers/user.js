cee2be726a0f0d78fb18059c49b0f052f5f4b1a7
ikota3 <51883336+ikota3@users.noreply.github.com>
2020-06-22 23:56:43 +0900

    Add multer lib and add upload avatar and delete avatar route

--- node/task-manager/src/routers/user.js
+++ node/task-manager/src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = new express.Router();
+const multer = require("multer");
 const User = require("../models/user");
 const auth = require("../middleware/auth");
 
@@ -46,6 +47,51 @@ router.post("/users/logoutAll", auth, async (req, res) => {
     res.status(200).send();
   } catch (e) {
     res.status(500).send();
+  }
+});
+
+/**
+ * UPLOAD AVATAR IMAGE
+ */
+const upload = multer({
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error("Please upload jpg, jpeg or png."));
+    }
+    cb(undefined, true);
+  },
+});
+router.post(
+  "/users/me/avatar",
+  auth,
+  upload.single("upload"),
+  async (req, res) => {
+    req.user.avatar = req.file.buffer;
+    try {
+      await req.user.save();
+      res.status(200).send();
+    } catch (e) {
+      res.status(500).send();
+    }
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
+
+/**
+ * DELETE AVATAR IMAGE
+ */
+router.delete("/users/me/avatar", auth, async (req, res) => {
+  try {
+    req.user.avatar = undefined;
+    await req.user.save();
+    res.status(200).send();
+  } catch (e) {
+    res.status(500).send(e);
   }
 });
 
