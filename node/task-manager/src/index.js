ba8af42cdaae26824ea2c0701e2f37a72e0e6082
ikota3 <51883336+ikota3@users.noreply.github.com>
2020-06-25 23:45:16 +0900

    Move secret key to config file. Fix npm run dev command for loading env file before executing.

--- node/task-manager/src/index.js
+++ node/task-manager/src/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Listening on ${port}`);
 });
