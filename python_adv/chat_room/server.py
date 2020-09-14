ebe0ad014224d8d602a1a4aa040e97e737a91de1
ikota3 <51883336+ikota3@users.noreply.github.com>
2020-09-14 21:43:43 +0900

    Update client.py and server.py

--- python_adv/chat_room/server.py
+++ python_adv/chat_room/server.py
@@ -3,47 +3,52 @@ from threading import Thread
 
 HOST = 'localhost'
 PORT = 8080
-clients = {}
-addresses = {}
+CLIENTS = {}
+ADDRESSES = {}
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((HOST, PORT))
 
 
-def accept_client_connection():
+def accept_client_connection() -> None:
     while True:
         client_conn, client_address = sock.accept()
-        addresses[client_conn] = client_address
+        ADDRESSES[client_conn] = client_address
         print(f'{client_address} has connected')
         msg = 'Welcome to the chat room, Please type your name to continue.'
         client_conn.send(msg.encode('utf8'))
 
-        Thread(target=handle_clients, args=(client_conn, client_address))
+        Thread(
+            target=handle_clients,
+            args=(
+                client_conn,
+                client_address)
+        ).start()
 
 
-def handle_clients(conn, address):
-    name = conn.recv(1024).decode()
+def handle_clients(conn, address) -> None:
+    name = conn.recv(1024).decode('utf8')
     msg = f'Welcome {name}. You can type #quit if you ever want to leave the chat room.'
-    conn.recv(bytes(msg, "utf8"))
+    conn.send(msg.encode('utf8'))
     msg = f'{name} has recently joined the chat room.'
     broadcast(msg)
-    clients[conn] = name
+    CLIENTS[conn] = name
 
     while True:
-        msg = conn.recv(1024)
-        if msg != bytes("#quit", "utf8"):
+        msg = conn.recv(1024).decode('utf8')
+        if msg != "#quit":
             broadcast(msg, f'{name}: ')
         else:
-            conn.send(bytes("#quit", "utf8"))
+            conn.send("#quit".encode('utf8'))
             conn.close()
-            del clients[conn]
+            del CLIENTS[conn]
             broadcast(f'{name} has left the chat room.')
             break
 
 
-def broadcast(message, prefix=""):
-    for client in clients:
-        client.send(bytes(f'{prefix}{message}', "utf8"))
+def broadcast(msg: str, prefix: str = "") -> None:
+    for client in CLIENTS:
+        client.send(f'{prefix}{msg}'.encode("utf8"))
 
 
 if __name__ == '__main__':
